d5857e8029b940522cf926c7e5f6031f7453ebae
Joe Quigley <joeyjtq@hotmail.com>
2025-11-22 14:15:00 +0000

    Initial commit: Spring running locally

---
+++ src/main/java/com/joe/joepetitions/JoepetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.joe.joepetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoepetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoepetitionsApplication.class, args);
+	}
+
+}
