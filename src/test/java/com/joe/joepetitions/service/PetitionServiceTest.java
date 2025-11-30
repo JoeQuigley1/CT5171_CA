4dba038fe3a2b0c49d092bb88783efb132a47cfe
Joe Quigley <joeyjtq@hotmail.com>
2025-11-30 18:36:07 +0000

    Add petition details and Signature test

--- src/test/java/com/joe/joepetitions/service/PetitionServiceTest.java
+++ src/test/java/com/joe/joepetitions/service/PetitionServiceTest.java
@@ -1,8 +1,11 @@
 package com.joe.joepetitions.service;
 
+import com.joe.joepetitions.model.Petition;
+import com.joe.joepetitions.model.Signature;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PetitionServiceTest {
 
@@ -24,6 +27,25 @@ public class PetitionServiceTest {
 
         assertEquals(3, results.size());
         assertEquals("It's only November and the potholes are massive.", results.get(0).getDescription());
+    }
+
+    @Test
+    void petitionDetails() {
+        PetitionService service = new PetitionService();
+
+        Petition petition = service.getPetitionById(1);
+
+        assertNotNull(petition);
+        assertEquals("Fix the roads", petition.getTitle());
+        assertEquals("It's only November and the potholes are massive.", petition.getDescription());
+        assertEquals("John", petition.getAuthor());
+
+
+        Signature signature = petition.getSignatures().get(0);
+        assertNotNull(signature);
+        assertEquals("Alice Doe", signature.getName());
+        assertEquals("alice@example.com", signature.getEmail());
+        assertEquals(12345678, signature.getPhoneNumber());
     }
 
 }
